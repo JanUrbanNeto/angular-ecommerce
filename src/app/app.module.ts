0348438b1b6d2726b84d9c13b49f4129883a313d
JanUrbanNeto <jan.urbanneto@gmail.com>
2024-05-19 17:21:19 -0300

    commit de domingo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgFor,
+    NgIf
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-table.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+}
